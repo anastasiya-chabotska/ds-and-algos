da527ed23bb7fa9f1be0825ae956a701a3f1130c
Anastasiya Chabotska <anc33139@gmail.com>
2023-05-24 21:42:44 -0400

    changed to impl and new impl with arrays

---
+++ stacks_and_queues/stack_using_array.js
@@ -0,0 +1,34 @@
+class Node {
+    constructor(value){
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  class Stack {
+    constructor(){
+      this.stack = []
+    }
+    peek() {
+      return this.stack[this.stack.length - 1];
+    }
+    push(value){
+      this.stack.push(value)
+    }
+    pop(){
+      this.stack.pop();
+    }
+    //isEmpty
+  }
+  
+  const myStack = new Stack();
+  console.log(myStack)
+  
+  
+  //Discord
+  //Udemy
+  //google
+
+
+
+  
