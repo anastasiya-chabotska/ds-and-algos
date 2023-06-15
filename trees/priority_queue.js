19d88f919865c10b9541e28680f556fd44fd43ae
Anastasiya Chabotska <anc33139@gmail.com>
2023-06-14 23:26:15 -0400

    start implementing priority queue

---
+++ trees/priority_queue.js
@@ -0,0 +1,13 @@
+class QElement{
+    constructor(element, priority){
+        this.element = element;
+        this.priority = priority;
+    }
+}
+
+
+class PriorityQueue {
+    constructor() {
+        this.items = []
+    }
+}
