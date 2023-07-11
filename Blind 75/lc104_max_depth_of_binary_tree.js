67824d2d76e10b5ecf4d45131c47bb114ff81f81
Anastasiya Chabotska <anc33139@gmail.com>
2023-07-10 22:55:13 -0400

    added iterative dfs and bfs

--- Blind 75/lc104_max_depth_of_binary_tree.js
+++ Blind 75/lc104_max_depth_of_binary_tree.js
@@ -17,6 +17,9 @@ function TreeNode(val, left, right) {
  * @param {TreeNode} root
  * @return {number}
  */
+
+//Time: O(n) Space: O(n) - if the tree is unbalanced im worst case
+//DFS
 var maxDepth = function(root) {
     
    if(root == null) return 0;
@@ -33,6 +36,54 @@ var maxDepth = function(root) {
 };
 
 
+//Iterative BFS 
+//BFS often involves queue
+var maxDepth2 = function(root) {
+    
+    if(root == null) return 0;
+
+    let queue = [root];
+    let level = 0;
+
+    while(queue.length > 0){
+
+        for(let i = 0; i<queue.length; i++){
+            let node = queue.shift();
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
+
+        level++;
+
+    }
+
+    return level;
+   
+     
+ };
+
+
+//Iterative DFS 
+//here use stack
+var maxDepth3 = function(root) {
+    
+    if(root == null) return 0
+    let stack = [[root, 1]];
+    let result = 1;
+
+    while(stack.length > 0){
+        let node = stack.pop();
+        // console.log(node[0].val)
+        result = Math.max(node[1], result)
+        if(node[0].right) stack.push([node[0].right, 1 + node[1]])
+        if(node[0].left) stack.push([node[0].left, 1 + node[1]])
+    }
+
+    return result;
+     
+ };
+
+
 let n1 = new TreeNode(3);
 let n2 = new TreeNode(9);
 let n3 = new TreeNode(20);
@@ -44,7 +95,7 @@ n1.right = n3;
 n3.left = n4;
 n3.right = n5;
 
-console.log(maxDepth(n1))
+console.log(maxDepth3(n1))
 
 
 
