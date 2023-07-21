f3362834f8ac2e62f226532ea72af20be39ff5a0
Anastasiya Chabotska <anc33139@gmail.com>
2023-07-20 23:12:58 -0400

    added notes for bitwise operators in js

---
+++ Blind 75/notes/bitwise_operators.js
@@ -0,0 +1,10 @@
+// 1 = 00000001
+// 8 bits
+// 2 = 00000010
+// R = 00000011
+//bitwise or goes vertically comparing
+//if you convert R, console log will give 3
+
+console.log(1 | 2); //Bitwise OR
+//logical OR is ||
+console.log(1 & 2); //Bitwise AND
