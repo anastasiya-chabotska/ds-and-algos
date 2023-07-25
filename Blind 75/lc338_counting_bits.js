09ec88fb4f001b90c6e199feb1dc24bea5ea43b7
Anastasiya Chabotska <anc33139@gmail.com>
2023-07-24 21:44:40 -0400

    solved counting bits

---
+++ Blind 75/lc338_counting_bits.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+
+    let dp = [0];
+    let offset = 1;
+    for(let i = 1; i<=n; i++){
+        //check if we got new most significant bit
+        if(offset * 2 == i) offset = i;
+        //calculate number of 1 bits
+        dp[i] = 1 + dp[i-offset]
+    }
+    return dp;
+    
+};
