de7310e029e967ff9e18c1b1d5c4b4b01ecd6de9
Anastasiya Chabotska <anc33139@gmail.com>
2023-08-02 22:56:24 -0400

    solved 2 sum 2

---
+++ Blind 75/lc167_two_sum_II.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let [left, right] = [0, numbers.length - 1];
+
+    while(left < right){
+        let sum = numbers[left] + numbers[right];
+        if(target < sum) right--;
+        if(target > sum) left++;
+        if(target == sum) return [left + 1, right + 1];
+    }
+};
