f7df4a60916601478af32752815458be5474f55e
Anastasiya Chabotska <anc33139@gmail.com>
2023-09-01 21:49:56 -0400

    added some notes for lc39

---
+++ Blind 75/lc39_combination_sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    
+};
+
+
+/*decision tree
+
+[2, 3, 6, 7] 7
+                                -
+                                         [2]                                     []
+
+                        [2, 2]                         [2] 
+
+            [2, 2, 2]               [2, 2]        [2, 3]  [2]
+            
+[2, 2, 2, 2]      [2, 2, 2]   [2, 2, 3]   [2, 2]
+
+*/
