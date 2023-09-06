0f9c386fb9aafb6fc721a978b9cc3505fc03fb38
Anastasiya Chabotska <anc33139@gmail.com>
2023-09-05 22:51:44 -0400

    added a dp solution for lc39

--- Blind 75/lc39_combination_sum.js
+++ Blind 75/lc39_combination_sum.js
@@ -35,6 +35,36 @@ var combinationSum = function(candidates, target) {
 };
 
 
+var combinationSum = function(candidates, target) {
+    
+  let dp = new Array(target+1).fill(null).map(()=>[]);
+  
+  //2
+  for (let c of candidates){
+      for(let i = 1; i < dp.length; i++){
+          if(c > i) continue;
+          if(c == i) {
+              
+              dp[i].push([c]);
+          }
+          else{
+              let r = i - c;
+              if(dp[r].length>0){
+                  for(let rem of dp[r]){
+                      //console.log(rem)
+                      dp[i].push(rem.concat([c]))
+                  }
+              }
+          }
+
+      }
+  }
+
+
+
+  return dp[target]
+};
+
 /*decision tree
 
 [2, 3, 6, 7] 7
