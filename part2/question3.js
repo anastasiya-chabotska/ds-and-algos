b9d76d847fc801e6078988a98bbf5a40a5b3dd18
Anastasiya Chabotska <anc33139@gmail.com>
2023-11-15 21:01:51 -0500

    added optimal solution q3

--- part2/question3.js
+++ part2/question3.js
@@ -1,29 +1,66 @@
-var trapBF = function(height) {
-    
+var trapBF = function (height) {
+
 
     let totalWater = 0;
 
-    for(let i = 0; i <height.length; i++){
+    for (let i = 0; i < height.length; i++) {
 
         let left = height[i];
         let right = height[i];
 
         let leftI = i;
-        while(leftI >= 0){
+        while (leftI >= 0) {
             left = Math.max(left, height[leftI]);
             leftI--;
         }
 
         let rightI = i;
-        while(rightI < height.length){
+        while (rightI < height.length) {
             right = Math.max(right, height[rightI]);
             rightI++;
         }
 
         let currentWater = Math.min(left, right) - height[i];
-        if(currentWater > 0)
-        totalWater = totalWater + currentWater;
+        if (currentWater > 0)
+            totalWater = totalWater + currentWater;
 
+    }
+
+    return totalWater;
+};
+
+var trap = function (height) {
+
+
+    let totalWater = 0;
+
+    let pLeft = 0;
+    let pRight = height.length - 1;
+    let maxLeft = height[pLeft];
+    let maxRight = height[pRight];
+
+    while (pLeft < pRight) {
+
+        let curLeft = height[pLeft];
+        let curRight = height[pRight];
+
+        if (curLeft <= curRight) {
+            if (curLeft > maxLeft) maxLeft = curLeft;
+            else {
+                let currentWater = maxLeft - curLeft;
+                totalWater += currentWater;
+            }
+            pLeft++;
+        }
+        else {
+            if (curRight > maxRight) maxRight = curRight;
+            else {
+                let currentWater = maxRight - curRight;
+                totalWater += currentWater;
+            }
+
+            pRight--;
+        }
     }
 
     return totalWater;
