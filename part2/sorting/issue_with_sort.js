a4b0885fe0fe6592144b66ad2933c1f7b587044a
Anastasiya Chabotska <anc33139@gmail.com>
2023-12-04 22:35:07 -0500

    some notes about sorting

---
+++ part2/sorting/issue_with_sort.js
@@ -0,0 +1,15 @@
+const letters = ['a', 'd', 'z', 'e', 'r', 'b']; //this will sort fine
+const basket = [2, 65, 34, 2, 1, 7, 8]; // this will sort based on character codes
+
+
+'65'.charCodeAt(0);
+
+//also the space and time can not be guaranteed
+
+//other languages characters are also not properly supported, ex., á
+const spanish = ['único', 'árbol', 'cosas', 'fútbol']
+spanish.sort(function(a, b){
+    return a.localeCompare(b);
+})
+
+
