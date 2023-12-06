e2c024d404c8fd0c2ca870a660c9d91314933d59
Anastasiya Chabotska <anc33139@gmail.com>
2023-12-05 20:17:03 -0500

    selection sort

---
+++ part2/sorting/selection_sort.js
@@ -0,0 +1,17 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[minIndex]) minIndex = j;
+        }
+        let temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+
+    }
+}
+
+selectionSort(numbers);
+console.log(numbers)
