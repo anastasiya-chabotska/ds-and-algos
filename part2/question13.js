390d5cd2ee1c93c25c3da333f95386370c2c4467
Anastasiya Chabotska <anc33139@gmail.com>
2023-12-09 19:26:01 -0500

    full solution

--- part2/question13.js
+++ part2/question13.js
@@ -1,5 +1,64 @@
-//my quciksort
 const quickSort = function(array, left, right){
+    if(left < right){
+        let partitionIdx = partition(array, left, right);
+        quickSort(array, left, partitionIdx - 1);
+        quickSort(array, partitionIdx + 1, right);
+    }
+}
+
+const partition = function(array, left, right){
+    let partitionValue = array[right];
+    let partitionIdx = left;
+    for(let j = left; j < right; j++){
+        if(array[j] < partitionValue){
+            swap(array, j, partitionIdx);
+            partitionIdx++;
+        }
+    }
+    swap(array, partitionIdx, right);
+    return partitionIdx;
+}
+
+const swap = function(array, i, j){
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
+const getKthLargest = function(array, k){
+    let indexToFind = array.length - k;
+    quickSort(array, 0, array.length - 1);
+    return array[indexToFind];
+}
+
+const nums = [5, 3, 1, 6, 4, 2];
+// quickSort(nums, 0, nums.length - 1);
+// console.log(nums);
+console.log(getKthLargest(nums, 2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//my quciksort
+const quickSort1 = function(array, left, right){
     if(right <= left) return array[left];
     let pivot = right;
 
@@ -23,6 +82,6 @@ const quickSort = function(array, left, right){
     
 }
 
-const nums = [5, 3, 1, 6, 4, 2];
-quickSort(nums, 0, nums.length - 1);
-console.log(nums);
+// const nums = [5, 3, 1, 6, 4, 2];
+// quickSort(nums, 0, nums.length - 1);
+// console.log(nums);
