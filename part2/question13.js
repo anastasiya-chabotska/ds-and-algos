d1bb3d30388fac0dd8361dda0d31548ed18e5cbb
Anastasiya Chabotska <anc33139@gmail.com>
2023-12-10 20:54:00 -0500

    solution for q13 with quickselect

--- part2/question13.js
+++ part2/question13.js
@@ -37,6 +37,33 @@ const nums = [5, 3, 1, 6, 4, 2];
 console.log(getKthLargest(nums, 2));
 
 
+const getKthLargest2 = function(array, k){
+    let indexToFind = array.length - k;
+    console.log({indexToFind})
+    quickSelect(array, 0, array.length - 1, indexToFind);
+    return array[indexToFind];
+
+}
+
+const quickSelect = function(array, left, right, indexToFind){
+    if(left < right){
+        let partitionIdx = partition(array, left, right);
+        console.log({partitionIdx})
+        // if (indexToFind < partitionIdx) quickSelect(array, left, partitionIdx - 1, indexToFind);
+        // else if(indexToFind > partitionIdx) quickSelect(array, partitionIdx + 1, right, indexToFind);
+        
+        //for tail recursion
+        if(indexToFind == partitionIdx) return array[partitionIdx];
+        else if(indexToFind < partitionIdx) return quickSelect(array, left, partitionIdx - 1, indexToFind)
+        else return quickSelect(array, partitionIdx + 1, right, indexToFind);
+    }
+}
+
+
+const nums2 = [5, 3, 1, 6, 4, 2];
+// quickSort(nums, 0, nums.length - 1);
+// console.log(nums);
+console.log(getKthLargest2(nums2, 2));
 
 
 
