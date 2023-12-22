de86f3a642d94c678d83c66bdfd0afa97ff2e7cc
Anastasiya Chabotska <anc33139@gmail.com>
2023-12-22 14:04:27 -0500

    correct q19 solution

--- part2/question19.js
+++ part2/question19.js
@@ -1,22 +1,8 @@
-var isValidBST = function(node, min, max) {
-    if(!node) return true;
- 
-    console.log(node.val)
-    console.log({min, max})
-    if(!min && !max) {
-    min = node.val;
-    max = node.val;
+var isValidBST = function (node, greater = -Infinity, less = +Infinity) {
+    if (!node) return true;
+
+    if (node.val > greater && node.val < less) {
+        return isValidBST(node.left, greater, node.val) && isValidBST(node.right, node.val, less)
     }
-    else {
- if(node.val <= min || node.val >= max) return false
-    }
-    //basic comparison
-    if((node.left && (node.left.val >= node.val)) || (node.right && (node.right.val <= node.val))) return false;
-    //comparison to min and max
-   
-    //call on left and right child
-    console.log(node.left, node.right)
-    if(node.left) return isValidBST(node.left, Math.min(min, node.val), Math.max(max, node.val))
-    if(node.right) return isValidBST(node.right, Math.min(node.val, min), Math.max(max, node.val))
- 
- };
+    else return false;
+};
