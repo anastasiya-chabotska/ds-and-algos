19b5ab02385685cc51f6ac05f2b1ff91e5f789f1
Anastasiya Chabotska <anc33139@gmail.com>
2023-12-25 19:20:37 -0500

    cleaned up bfs

--- part2/bfs_2d_arr.js
+++ part2/bfs_2d_arr.js
@@ -8,30 +8,22 @@ const directions = [
 const traversalBFS = function(matrix){
     const seen = new Array(matrix.length).fill(0).map(()=>new Array(matrix[0].length).fill(false));
     const result = [];
-    const queue = [[2,2]];
+    const queue = [[0,0]];
     while(queue.length > 0){
         const curPos = queue.shift();
-        console.log(curPos, queue)
         const row = curPos[0];
         const col = curPos[1];
         if(row < 0 || col < 0 || row >= matrix.length || col >= matrix[0].length || seen[row][col]){
-
+            continue;
         }
-        else{
+      
         const curVal = matrix[curPos[0]][curPos[1]];
         result.push(curVal);
         seen[curPos[0]][curPos[1]] = true;
-        }
+        
         for(let i = 0; i < directions.length; i++){
             const curDir = directions[i];
-            const row = curPos[0] + curDir[0];
-            const col = curPos[1] + curDir[1];
-            if(row < 0 || col < 0 || row >= matrix.length || col >= matrix[0].length || seen[row][col]) continue;
-            else {
-                queue.push([row, col]);
-                result.push(matrix[row][col]);
-                seen[row][col] = true;
-            }
+            queue.push([row + curDir[0], col + curDir[1]])
             
         }
     }
