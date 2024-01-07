d787323c543d235d07f48376e37f3b7cd97bea63
Anastasiya Chabotska <anc33139@gmail.com>
2024-01-06 21:30:26 -0500

    solved q21

--- part2/question21.js
+++ part2/question21.js
@@ -1,8 +1,10 @@
-var orangesRotting = function(grid) {
+//my first attempts, incorrect
+
+var orangesRotting = function (grid) {
     let maxTime = 0;
-    for(let row = 0; row < grid.length; row++){
-        for(let col = 0; col < row[0].length; col++){
-            if(grid[row][col]==2){
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            if (grid[row][col] == 2) {
                 //traverse the adjacent oranges and calculate the time
                 let time = traverseAndRot(grid, row, col);
                 maxTime = Math.max(time, maxTime);
@@ -10,9 +12,9 @@ var orangesRotting = function(grid) {
         }
     }
 
-    for(let row = 0; row < grid.length; row++){
-        for(let col = 0; col < row[0].length; col++){
-            if(grid[row][col]==1){
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            if (grid[row][col] == 1) {
                 return -1;
             }
         }
@@ -20,28 +22,45 @@ var orangesRotting = function(grid) {
     return maxTime;
 };
 
-var directions = [
+const directions = [
     [-1, 0],
     [0, 1],
     [1, 0],
     [0, -1]
 ]
 
-var traverseAndRot = function(grid, row, col){
+var traverseAndRot = function (grid, row, col) {
     let queue = [[row, col]];
-    let time = 0;
-    while(queue.length){
-        let curCellPos = queue.shift();
-        let curRow = curCellPos[0];
-        let curCol = curCellPos[1];
-        grid[curRow][curCol] = 2;
-        for(let i = 0; i < directions.length; i++){
-            let newRow = curRow + directions[i][0];
-            let newCol = curCol + directions[i][1];
-            if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && grid[newRow][newCol]==1 ){
-                queue.push([newRow, newCol])
+    let time = -1;
+    while (queue.length) {
+        let curLength = queue.length;
+        while (curLength > 0) {
+            let curCellPos = queue.shift();
+            let curRow = curCellPos[0];
+            let curCol = curCellPos[1];
+            grid[curRow][curCol] = 2;
+            for (let i = 0; i < directions.length; i++) {
+                let newRow = curRow + directions[i][0];
+                let newCol = curCol + directions[i][1];
+                if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && grid[newRow][newCol] == 1) {
+                    queue.push([newRow, newCol])
+                }
             }
+            curLength--;
         }
+        time++;
+        console.log("time ", time, queue)
+
 
     }
+    return time;
 }
+
+
+let grid = [[0,2]];
+console.log(orangesRotting(grid));
+
+
+
+
+
---
+++ part2/question21a.js
@@ -0,0 +1,56 @@
+var orangesRotting = function (grid) {
+    //find rotten indices, and count of fresh
+    let { countFresh, rottenIdx } = findRottendIndeces(grid);
+    //rot out
+    let {minutes, freshAfterRot} = rotOut(grid, rottenIdx, countFresh);
+    //check if fresh is at 0, if yes, return minutes, else -1
+    return freshAfterRot > 0 ? -1 : minutes;
+}
+
+var findRottendIndeces = function (grid) {
+    let countFresh = 0;
+    let rottenIdx = [];
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            if (grid[row][col] == 2) {
+                rottenIdx.push([row, col])
+            }
+            if (grid[row][col] == 1) {
+                countFresh++;
+            }
+        }
+    }
+    return { countFresh, rottenIdx }
+}
+
+var directions = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1]
+]
+
+var rotOut = function (grid, rottenIdx, countFresh) {
+    let minutes = 0;
+    let queue = rottenIdx;
+    let freshAfterRot = countFresh
+    while (queue.length) {
+        let tempLength = queue.length;
+        while (tempLength > 0) {
+            let curPos = queue.shift();
+            for (let i = 0; i < directions.length; i++) {
+                let newRow = curPos[0] + directions[i][0];
+                let newCol = curPos[1] + directions[i][1];
+                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && grid[newRow][newCol]==1){
+                    freshAfterRot--;
+                    grid[newRow][newCol] = 2;
+                    queue.push([newRow, newCol]);
+                }
+            }
+            tempLength--;
+        }
+        minutes++;
+    }
+    if(minutes > 0) minutes--;
+    return {minutes, freshAfterRot}
+}
