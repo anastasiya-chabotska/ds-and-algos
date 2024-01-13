b891685a1dd3ada479e328b691cd41854745e700
Anastasiya Chabotska <anc33139@gmail.com>
2024-01-12 19:50:05 -0500

    added bfs for adjacency matrix

--- part2/bfs_graph.js
+++ part2/bfs_graph.js
@@ -17,11 +17,32 @@ const bfs = function(graph){
   
 }
 
+const bfs2 = function(graph){
+    const queue = [0];
+    const result = [];
+    const seen = {};
+    while(queue.length){
+        const vertex = queue.shift();
+        result.push(vertex);
+        seen[vertex] = true;
+        const connections = graph[vertex];
+        console.log({vertex})
+        for(let i = 0; i < connections.length; i++){
+            const connection = connections[i];
+            if(connection == 1) console.log({i})
+            if(connection == 1 && !seen[i]) queue.push(i)
+        }
+
+    }
+    return result;
+
+}
+
 const adjacencyList = [
     [1, 3],
     [0],
     [3, 8],
-    [0, 4, 5, 2],
+    [0, 2, 4, 5],
     [3, 6],
     [3],
     [4, 7],
@@ -29,4 +50,18 @@ const adjacencyList = [
     [2]
 ]
 
+
+const adjacencyMatrix = [
+    [0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0]
+]
+
 console.log(bfs(adjacencyList))
+console.log(bfs2(adjacencyMatrix))
