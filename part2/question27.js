45345207f55b85d656369fc37c61c6a3ea1decb0
Anastasiya Chabotska <anc33139@gmail.com>
2024-02-12 21:38:43 -0500

    q27 optimized with dp

--- part2/question27.js
+++ part2/question27.js
@@ -19,3 +19,31 @@ var knightProbability = function (n, k, row, col) {
     return knightP / 8;
 
 };
+
+/*
+n = 3
+[false, false, false]
+[false, false, false]
+[false, false, false]
+
+
+*/
+//optimize with dp
+var knightProbability2 = function (n, k, row, col, dp = new Array(n).fill(0).map(()=>new Array(n).fill(0).map(()=>({})))) {
+    if (row < 0 || row >= n || col < 0 || col >= n) return 0;
+    if(k === 0) return 1;
+    let res = 0;
+
+    if(dp[row][col][k]) {
+        return dp[row][col][k];
+    }
+    for(let i = 0; i < directions.length; i++){
+        const dir = directions[i];
+        res += knightProbability2(n, k - 1, row + dir[0], col + dir[1], dp)/8;
+        
+        
+    }
+    dp[row][col][k] = res;
+    return res;
+
+};
