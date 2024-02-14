f16d3e9a5c5dcface4364763ec8f80fd3150b8be
Anastasiya Chabotska <anc33139@gmail.com>
2024-02-13 22:20:36 -0500

    q27 bottom up approach

--- part2/question27.js
+++ part2/question27.js
@@ -29,21 +29,58 @@ n = 3
 
 */
 //optimize with dp
-var knightProbability2 = function (n, k, row, col, dp = new Array(n).fill(0).map(()=>new Array(n).fill(0).map(()=>({})))) {
+var knightProbability2 = function (n, k, row, col, dp = new Array(n).fill(0).map(() => new Array(n).fill(0).map(() => ({})))) {
     if (row < 0 || row >= n || col < 0 || col >= n) return 0;
-    if(k === 0) return 1;
+    if (k === 0) return 1;
     let res = 0;
 
-    if(dp[row][col][k]) {
+    if (dp[row][col][k]) {
         return dp[row][col][k];
     }
-    for(let i = 0; i < directions.length; i++){
+    for (let i = 0; i < directions.length; i++) {
         const dir = directions[i];
-        res += knightProbability2(n, k - 1, row + dir[0], col + dir[1], dp)/8;
-        
-        
+        res += knightProbability2(n, k - 1, row + dir[0], col + dir[1], dp) / 8;
+
+
     }
     dp[row][col][k] = res;
     return res;
 
 };
+
+//bottom up
+var knightProbability3 = function (n, k, r, c) {
+    const dp = new Array(k + 1).fill(0).map(() => new Array(n).fill(0).map(() => new Array(n).fill(0)))
+    console.log(dp)
+    dp[0][r][c] = 1;
+
+    for (let i = 1; i <= k; i++) {
+        const level = dp[i];
+        for (let row = 0; row < n; row++) {
+            for (let col = 0; col < n; col++) {
+                const probability = getProbability(n, i, row, col, dp);
+                level[row][col] = probability;
+            }
+        }
+    }
+    console.log(dp)
+
+};
+
+var getProbability = function (n, k, r, c, dp) {
+    const prevLevel = dp[k - 1];
+    let res = 0;
+    console.log({ k, prevLevel })
+    for (let i = 0; i < directions.length; i++) {
+        const dir = directions[i];
+        const newRow = r + dir[0];
+        const newCol = c + dir[1];
+        console.log({ newRow, newCol })
+        if (newRow < 0 || newCol < 0 || newRow >= n || newCol >= n) continue;
+        else res += prevLevel[newRow][newCol];
+    }
+    console.log(res / 8)
+    return res / 8;
+}
+
+knightProbability3(3, 2, 0, 0);
