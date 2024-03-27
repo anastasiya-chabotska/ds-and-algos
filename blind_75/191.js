a6b7e007d6ba1bfc6d742028fcbb93acff7b3265
Anastasiya Chabotska <anc33139@gmail.com>
2024-03-26 21:59:39 -0400

    added better solutions 191

--- blind_75/191.js
+++ blind_75/191.js
@@ -1,7 +1,27 @@
 var hammingWeight = function (n) {
     let res = 0;
-    for (let i = 0; i < 32; i++) {
-        if ((n & Math.pow(2, i)) > 0) res++;
+    while (n > 0) {
+        n = n & (n - 1)
+        res++
     }
     return res;
 };
+
+
+// var hammingWeight = function(n) {
+//     let res = 0;
+//     while(n > 0){
+//         if(n%2 > 0) res++;
+//         n = n >> 1;
+//     }
+//     return res;
+// };
+
+// my solution 
+// var hammingWeight = function(n) {
+//     let res = 0;
+//     for(let i =0; i < 32; i++){
+//         if((n & Math.pow(2, i))>0) res++;
+//     }
+//     return res;
+// };
